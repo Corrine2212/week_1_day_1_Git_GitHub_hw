580a23813dac5b29acb34db8e480335104511d0c
Corrine Sing <corrine.sing@gmail.com>
2023-03-20 15:23:35 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,6 @@
+favourite_movie = "A League of Their Own"
+favourite_movie
+release_year = 1992
+lead_actor = "Geena Davis"
+
+print("My favourite movie is " + favourite_movie + ", it was released in " + str(release_year) + " and stars " + lead_actor + ".")
